fa32b23f112bb87ee4000335a3e3c377ce98c541
Julian Eberius <julian@eberius-mail.de>
2011-04-19 19:34:26 +0200

    cleanup of old code

--- osxwindows/osxaccessibility.m
+++ osxwindows/osxaccessibility.m
@@ -63,7 +63,7 @@ static bool amIAuthorized ()
     return false;
 }
 
-static char* to_c_string(CFStringRef str) 
+static char* toCString(CFStringRef str) 
 {
   char *result = CFStringGetCStringPtr(str, kCFStringEncodingUTF8);
   if(!result) {
@@ -78,7 +78,7 @@ static char* to_c_string(CFStringRef str)
 }
 
 
-static CFArrayRef getWindowsForApp(AXUIElementRef app)
+static CFArrayRef windowsForApp(AXUIElementRef app)
 {
   CFArrayRef windows;
   AXUIElementCopyAttributeValue(
@@ -117,7 +117,7 @@ static PyObject* fillWindowInformation(CFArrayRef windows, PyObject* windowList)
     if (windowTitle==NULL)
       windowTitle = CFSTR("");
     PyObject* window_tuple = PyTuple_Pack(5,
-      PyString_FromString(to_c_string(windowTitle)), 
+      PyString_FromString(toCString(windowTitle)), 
       PyFloat_FromDouble(windowPosition.x),
       PyFloat_FromDouble(windowPosition.y),
       PyFloat_FromDouble(windowSize.width),
@@ -135,7 +135,7 @@ static PyObject* copyWindowsInformationForPID(pid_t pid)
 {
   PyObject* result = NULL;
   AXUIElementRef app = AXUIElementCreateApplication(pid);
-  CFArrayRef windows = getWindowsForApp(app);
+  CFArrayRef windows = windowsForApp(app);
   if (windows != NULL) 
   {
     if (CFArrayGetCount(windows) > 0) 
@@ -151,111 +151,3 @@ static PyObject* copyWindowsInformationForPID(pid_t pid)
     result = PyList_New(0);
   return result;
 }
-
-// static PyObject* getWindowedApps() 
-// {
-//   PyObject* app_pids = getListOfAppPIDs();
-//   PyObject* windowed_apps = PyDict_New();
-//   int app_count = PyList_Size(app_pids);
-//   int i;
-//   pid_t pid;
-//   ProcessSerialNumber psn;
-//   for (i=0; i<app_count; i++) 
-//   {
-//     pid = PyInt_AS_LONG( PyList_GetItem(app_pids,i) );
-//     AXUIElementRef app = AXUIElementCreateApplication(pid);
-    
-//     CFArrayRef windows = getWindowsForApp(app);
-//     if (windows != NULL) 
-//     {
-//       if (CFArrayGetCount(windows) > 0) 
-//       {
-//         GetProcessForPID(pid, &psn);
-//         CFDictionaryRef processInfoDict = ProcessInformationCopyDictionary(&psn, kProcessDictionaryIncludeAllInformationMask);
-//         CFStringRef name = CFDictionaryGetValue(processInfoDict, CFSTR("CFBundleName"));
-//         CFStringRef identifier = CFDictionaryGetValue(processInfoDict, CFSTR("CFBundleIdentifier"));
-//         CFRelease(processInfoDict);
-
-//         PyObject* app_name = PyString_FromString(to_c_string(name));
-//         PyObject* app_identifier = PyString_FromString(to_c_string(identifier));
-//         PyObject* window_objects = PyList_New(CFArrayGetCount(windows));
-//         fillWindowInformation(windows, window_objects);
-//         PyObject* app_object = PyTuple_Pack(2, 
-//           app_name, 
-//           window_objects);
-//         PyDict_SetItem(windowed_apps, app_identifier, app_object);
-
-//         Py_DECREF(app_name);
-//         Py_DECREF(app_identifier);
-//         Py_DECREF(window_objects);
-//         Py_DECREF(app_object);
-//       }
-//       CFRelease(windows);
-//     }
-//     CFRelease(app);
-//   }
-//   Py_DECREF(app_pids);
-//   return windowed_apps;
-// }
-
-// static PyObject* getListOfAppPIDs()
-// {
-//   pid_t pid;
-//   ProcessSerialNumber psn = { 0, kNoProcess };
-
-//   PyObject* list = PyList_New(0);
-//   while (true)
-//   {
-//     GetNextProcess(&psn);      
-//     if (psn.lowLongOfPSN == kNoProcess)
-//       break;
-//     GetProcessPID(&psn, &pid);
-//     PyObject* tmp = Py_BuildValue("i", pid);
-//     PyList_Append(list, tmp);
-//     Py_DECREF(tmp);
-//   }
-
-//   return list;
-// }
-
-// static PyObject* getListOfAppNames()
-// {
-//   ProcessSerialNumber psn = { 0, kNoProcess };
-//   CFDictionaryRef processInfoDict = NULL;
-
-//   PyObject* list = PyList_New(0);
-//   while (true)
-//   {
-//     GetNextProcess(&psn);      
-//     if (psn.lowLongOfPSN == kNoProcess)
-//       break;
-
-//     processInfoDict = ProcessInformationCopyDictionary(&psn, kProcessDictionaryIncludeAllInformationMask);
-//     CFStringRef name = CFDictionaryGetValue(processInfoDict, CFSTR("CFBundleName"));
-//     char* buf = to_c_string(name);
-//     CFRelease(processInfoDict);
-
-//     PyObject* tmp = Py_BuildValue("s", buf);
-//     PyList_Append(list, tmp);
-//     Py_DECREF(tmp);
-//   }
-
-//   return list;
-// }
-
-// static AXUIElementRef getFrontMostApp()
-// {
-//     pid_t pid;
-//     ProcessSerialNumber psn;
-
-//     GetFrontProcess(&psn);
-//     GetProcessPID(&psn, &pid);
-//     return AXUIElementCreateApplication(pid);
-// }
-
-// static AXUIElementRef getAppForPSN(ProcessSerialNumber psn)
-// {
-//     pid_t pid;
-//     GetProcessPID(&psn, &pid);
-//     return AXUIElementCreateApplication(pid);
-// }
