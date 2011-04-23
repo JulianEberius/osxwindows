8815deac41128e1f93ad9a0254170be1a13b2f6a
Julian Eberius <julian@eberius-mail.de>
2011-04-23 12:00:33 +0200

    added methods for moving and resizing windows

--- osxwindows/__init__.py
+++ osxwindows/__init__.py
@@ -16,24 +16,51 @@ class Layout(object):
 
 class Window(object):
     """represents a OSX-Window, with position, size and title"""
-    def __init__(self, position, size, title):
+    def __init__(self, position, size, title, window_ref):
         super(Window, self).__init__()
-        self.position = position
-        self.size = size
+        self.x = position[0]
+        self.y = position[1]
+        self.width = size[0]
+        self.height = size[1]
         self.title = title
-        
-def get_windowed_apps():
-    windowed_apps_raw = osxaccessibility.list_windowed_apps()
+        self._window_ref = window_ref
+
+    def move_by(self,x,y):
+         osxaccessibility.move_window_by(self._window_ref,x,y)
+         self.x += x
+         self.y += y
+    def move_to(self,x,y):
+         osxaccessibility.move_window_to(self._window_ref,x,y)
+         self.x = x
+         self.y = y
+    def resize_by(self,width,height):
+         osxaccessibility.resize_window_by(self._window_ref,width,height)
+         self.width += width
+         self.height += height
+    def resize_to(self,width,height):
+         osxaccessibility.resize_window_to(self._window_ref,width,height)
+         self.width = width
+         self.height = height
+
+
+def windowed_apps():
+    """ returns all apps that would be returned by [NSWorkspace launchedApplicatios],
+    e.g., Apps on the Dock """
+    windowed_apps_raw = osxaccessibility.windowed_apps_raw()
     apps = []
     for identifier, name, windows_raw in windowed_apps_raw:
-        windows = [Window((w[1], w[2]), (w[3], w[4]), w[0])
+        windows = [Window((w[1], w[2]), (w[3], w[4]), w[0], w[5])
                     for w in windows_raw]
         app = App(identifier, name, windows)
         apps.append(app)
     return apps
 
+def screen_size():
+    """ return the current screen's size as a (width,height) tuple """
+    return osxaccessibility.screen_size()
+
 if __name__ == '__main__':
-    apps = get_windowed_apps()
+    apps = windowed_apps()
     for a in apps:
         print a.name
         for w in a.windows:
--- osxwindows/osxaccessibility.m
+++ osxwindows/osxaccessibility.m
@@ -2,10 +2,62 @@
 #include <Carbon/Carbon.h>
 #include <Cocoa/Cocoa.h>
 
+typedef struct {
+  PyObject_HEAD
+  /* Type-specific fields go here. */
+  AXUIElementRef window;
+} WindowReference;
+
+static void
+WindowReference_dealloc(WindowReference* self)
+{
+    CFRelease(self->window);
+    self->ob_type->tp_free((PyObject*)self);
+}
+
+static PyTypeObject WindowReferenceType = {
+  PyObject_HEAD_INIT(NULL)
+  0,        /* ob_size        */
+  "osxaccessibility.WindowReference",    /* tp_name        */
+  sizeof(WindowReference),  /* tp_basicsize   */
+  0,        /* tp_itemsize    */
+  (destructor)WindowReference_dealloc,        /* tp_dealloc     */
+  0,        /* tp_print       */
+  0,        /* tp_getattr     */
+  0,        /* tp_setattr     */
+  0,        /* tp_compare     */
+  0,        /* tp_repr        */
+  0,        /* tp_as_number   */
+  0,        /* tp_as_sequence */
+  0,        /* tp_as_mapping  */
+  0,        /* tp_hash        */
+  0,        /* tp_call        */
+  0,        /* tp_str         */
+  0,        /* tp_getattro    */
+  0,        /* tp_setattro    */
+  0,        /* tp_as_buffer   */
+  Py_TPFLAGS_DEFAULT,   /* tp_flags       */
+  "Stores a accessibility API reference to a window.", /* tp_doc         */
+};
+
+static PyObject* authorized(PyObject *self, PyObject *args)
+{
+    if (AXAPIEnabled() != 0) {
+        /* Yehaa, all apps are authorized */
+        Py_RETURN_TRUE;
+    }
+    /* Bummer, it's not activated, maybe we are trusted */
+    if (AXIsProcessTrusted() != 0) {
+        /* Good news, we are already trusted */
+        Py_RETURN_TRUE;
+    }
+    Py_RETURN_FALSE;
+}
+
 static PyObject* copyWindowsInformationForPID(pid_t pid);
 
 static PyObject *
-list_windowed_apps(PyObject *self, PyObject *args) {
+windowed_apps_raw(PyObject *self, PyObject *args) {
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
   NSWorkspace* ws = [NSWorkspace sharedWorkspace];
@@ -23,7 +75,7 @@ list_windowed_apps(PyObject *self, PyObject *args) {
     PyObject* window_list = copyWindowsInformationForPID(pid);
 
     PyObject* app_object = PyTuple_Pack(3,
-      app_identifier, 
+      app_identifier,
       app_name,
       window_list);
     PyList_SetItem(result, i, app_object);
@@ -35,37 +87,145 @@ list_windowed_apps(PyObject *self, PyObject *args) {
   return result;
 }
 
-PyMethodDef methods[] = {
-  {"list_windowed_apps", list_windowed_apps, METH_VARARGS, "Returns a list of open windows"},
+static PyObject *
+move_window_by(PyObject *self, PyObject *args)
+{
+  PyObject* window;
+  int x,y;
+  PyArg_ParseTuple(args, "Oii", &window, &x, &y);
+
+  AXUIElementRef windowRef = ((WindowReference*)window)->window;
+
+  AXValueRef temp;
+  CGPoint windowPosition;
+
+  AXUIElementCopyAttributeValue(
+      windowRef, kAXPositionAttribute, (CFTypeRef *)&temp
+  );
+  AXValueGetValue(temp, kAXValueCGPointType, &windowPosition);
+  CFRelease(temp);
+
+  windowPosition.y += y;
+  windowPosition.x += x;
+  temp = AXValueCreate(kAXValueCGPointType, &windowPosition);
+  AXUIElementSetAttributeValue(windowRef, kAXPositionAttribute, temp);
+  CFRelease(temp);
+
+  Py_RETURN_TRUE;
+}
+
+static PyObject *
+move_window_to(PyObject *self, PyObject *args)
+{
+  PyObject* window;
+  int x,y;
+  PyArg_ParseTuple(args, "Oii", &window, &x, &y);
+
+  AXUIElementRef windowRef = ((WindowReference*)window)->window;
+  AXValueRef temp;
+  CGPoint windowPosition;
+
+  windowPosition = CGPointMake(x,y);
+  temp = AXValueCreate(kAXValueCGPointType, &windowPosition);
+  AXUIElementSetAttributeValue(windowRef, kAXPositionAttribute, temp);
+  CFRelease(temp);
+
+  Py_RETURN_TRUE;
+}
+
+static PyObject *
+resize_window_by(PyObject *self, PyObject *args)
+{
+  PyObject* window;
+  int width,height;
+  PyArg_ParseTuple(args, "Oii", &window, &width, &height);
+
+  AXUIElementRef windowRef = ((WindowReference*)window)->window;
+
+  AXValueRef temp;
+  CGSize windowSize;
+
+  AXUIElementCopyAttributeValue(
+      windowRef, kAXSizeAttribute, (CFTypeRef *)&temp
+  );
+  AXValueGetValue(temp, kAXValueCGSizeType, &windowSize);
+  CFRelease(temp);
+
+  windowSize.width += width;
+  windowSize.height += height;
+  temp = AXValueCreate(kAXValueCGSizeType, &windowSize);
+  AXUIElementSetAttributeValue(windowRef, kAXSizeAttribute, temp);
+  CFRelease(temp);
+
+  Py_RETURN_TRUE;
+}
+
+static PyObject *
+resize_window_to(PyObject *self, PyObject *args)
+{
+  PyObject* window;
+  int width,height;
+  PyArg_ParseTuple(args, "Oii", &window, &width, &height);
+
+  AXUIElementRef windowRef = ((WindowReference*)window)->window;
+
+  AXValueRef temp;
+  CGSize windowSize;
+
+  windowSize = CGSizeMake(width,height);
+  temp = AXValueCreate(kAXValueCGSizeType, &windowSize);
+  AXUIElementSetAttributeValue(windowRef, kAXSizeAttribute, temp);
+  CFRelease(temp);
+
+  Py_RETURN_TRUE;
+}
+
+static PyObject *
+screen_size(PyObject *self, PyObject *args)
+{
+    NSRect screen_size = [[NSScreen mainScreen] frame];
+    PyObject* py_screen_size = PyTuple_Pack(2,
+            Py_BuildValue("i",(int)screen_size.size.width),
+            Py_BuildValue("i",(int)screen_size.size.height)
+            );
+    return py_screen_size;
+}
+
+
+static PyMethodDef methods[] = {
+  {"authorized", authorized, METH_VARARGS, "Checks whether the app is allowed to access the accessibility API."},
+  {"windowed_apps_raw", windowed_apps_raw, METH_VARARGS, "Returns a list of open windows"},
+  {"move_window_by", move_window_by, METH_VARARGS, "Moves a window by x and y pixel."},
+  {"move_window_to", move_window_to, METH_VARARGS, "Moves a window to x and y pixel."},
+  {"resize_window_by",resize_window_by, METH_VARARGS, "Resizes a window by x and y pixel."},
+  {"resize_window_to", resize_window_to, METH_VARARGS, "Resizes a window to x and y pixel."},
+  {"screen_size", screen_size, METH_VARARGS, "Returns the screen size as tuple (width,height)."},
   {NULL, NULL, 0, NULL}
 };
 
-PyMODINIT_FUNC 
-initosxaccessibility()
+PyMODINIT_FUNC
+initosxaccessibility(void)
 {
-  (void) Py_InitModule("osxaccessibility", methods);   
+  PyObject* m;
+
+  WindowReferenceType.tp_new = PyType_GenericNew;
+  if (PyType_Ready(&WindowReferenceType) < 0)
+    return;
+
+  m = Py_InitModule("osxaccessibility", methods);
+  if (m == NULL)
+    return;
+
+  Py_INCREF(&WindowReferenceType);
+  PyModule_AddObject(m, "WindowReference", (PyObject *)&WindowReferenceType);
+
 }
 
-/*-------------- C-API-Binding ---------------------*/
+/*-------------- Utility Functions ---------------------*/
 
-
-static bool amIAuthorized ()
+static char* toCString(CFStringRef str)
 {
-    if (AXAPIEnabled() != 0) {
-        /* Yehaa, all apps are authorized */
-        return true;
-    }
-    /* Bummer, it's not activated, maybe we are trusted */
-    if (AXIsProcessTrusted() != 0) {
-        /* Good news, we are already trusted */
-        return true;
-    }
-    return false;
-}
-
-static char* toCString(CFStringRef str) 
-{
-  char *result = CFStringGetCStringPtr(str, kCFStringEncodingUTF8);
+  char *result = (char*)CFStringGetCStringPtr(str, kCFStringEncodingUTF8);
   if(!result) {
     result = alloca(PATH_MAX);
     if(result)
@@ -77,6 +237,7 @@ static char* toCString(CFStringRef str)
   return result;
 }
 
+/*-------------- C-API-Binding ---------------------*/
 
 static CFArrayRef windowsForApp(AXUIElementRef app)
 {
@@ -87,7 +248,7 @@ static CFArrayRef windowsForApp(AXUIElementRef app)
   return windows;
 }
 
-static PyObject* fillWindowInformation(CFArrayRef windows, PyObject* windowList)
+static void fillWindowInformation(CFArrayRef windows, PyObject* windowList)
 {
   AXValueRef temp;
   CGSize windowSize;
@@ -114,16 +275,22 @@ static PyObject* fillWindowInformation(CFArrayRef windows, PyObject* windowList)
     AXValueGetValue(temp, kAXValueCGPointType, &windowPosition);
     CFRelease(temp);
 
+    WindowReference* windowRef = (WindowReference*)PyObject_CallFunction(
+      (PyObject*)&WindowReferenceType, NULL);
+    windowRef->window = w;
+    CFRetain(w);
+
     if (windowTitle==NULL)
       windowTitle = CFSTR("");
-    PyObject* window_tuple = PyTuple_Pack(5,
-      PyString_FromString(toCString(windowTitle)), 
+    PyObject* window_tuple = PyTuple_Pack(6,
+      PyString_FromString(toCString(windowTitle)),
       PyFloat_FromDouble(windowPosition.x),
       PyFloat_FromDouble(windowPosition.y),
       PyFloat_FromDouble(windowSize.width),
-      PyFloat_FromDouble(windowSize.height));
+      PyFloat_FromDouble(windowSize.height),
+      windowRef);
     int x;
-    for (x=0;x<5;x++)
+    for (x=0;x<6;x++)
       Py_DECREF(PyTuple_GetItem(window_tuple,x));
 
     PyList_SetItem(windowList, i, window_tuple);
@@ -131,14 +298,14 @@ static PyObject* fillWindowInformation(CFArrayRef windows, PyObject* windowList)
 }
 
 // TODO: throw errors! just returns [] at the moment
-static PyObject* copyWindowsInformationForPID(pid_t pid) 
+static PyObject* copyWindowsInformationForPID(pid_t pid)
 {
   PyObject* result = NULL;
   AXUIElementRef app = AXUIElementCreateApplication(pid);
   CFArrayRef windows = windowsForApp(app);
-  if (windows != NULL) 
+  if (windows != NULL)
   {
-    if (CFArrayGetCount(windows) > 0) 
+    if (CFArrayGetCount(windows) > 0)
     {
       result = PyList_New(CFArrayGetCount(windows));
       fillWindowInformation(windows, result);
